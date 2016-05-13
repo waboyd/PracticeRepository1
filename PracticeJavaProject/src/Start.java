501cd551efc8374ef450c921abd33e05600354c6
Hessam <hssarjoughian@gmail.com>
2016-05-12 23:55:49 -0700

    A simple test of making a change to an existing Java class.

--- PracticeJavaProject/src/Start.java
+++ PracticeJavaProject/src/Start.java
@@ -3,5 +3,6 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println("The program completed execution.");
+		System.out.println("added -- The program completed execution.");
 	}
 }
